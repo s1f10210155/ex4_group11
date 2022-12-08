2fb58daebe54f2e3448bdda285f4b60d60aff9c8
s1f10210155 <s1f102101558@iniad.org>
2022-12-08 16:27:30 +0900

    chat delete

--- django/config/urls.py
+++ django/config/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("home/", include("home.urls")),
+    path("chat/", include("chat.urls")),
     path('admin/', admin.site.urls),
 ]
