d862fd2613380e0ed708b04fce48e0304ea94cba
s1f10210155 <s1f102101558@iniad.org>
2023-01-05 15:29:41 +0900

    display threads

--- django/home/urls.py
+++ django/home/urls.py
@@ -4,8 +4,8 @@ from . import views
 #参考：https://houdoukyokucho.com/2020/05/22/post-1025/
 
 urlpatterns = [
-    path("", views.pub_home, name="pub_home"),
-    path("display_threads/", views.display_threads, name="display_threads"),
+    path("", views.display_threads, name="pub_home"),
+    #path("display_threads/", views.display_threads, name="display_threads"),
     path("display_comments/<int:thread_id>/", views.display_comments, name="display_comments"),
 
     path("create_room", views.create_room, name="create_room"),
